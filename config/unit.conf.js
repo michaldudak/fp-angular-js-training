8d44c14f05fecd509766716349543ede7efd89f2
doodack <michal.dudak@gmail.com>
2014-04-10 22:43:22 +0200

    Added missing scripts to test config

--- config/unit.conf.js
+++ config/unit.conf.js
@@ -12,9 +12,11 @@
 			"public/scripts/libs/jquery.js",
 			"public/scripts/libs/angular.js",
 			"public/scripts/libs/angular-route.js",
+			"public/scripts/libs/angular-resource.js",
 			"public/scripts/libs/angular-mocks.js",
 			"public/scripts/gallery/module.js",
 			"public/scripts/gallery/*.js",
+			"public/scripts/app.js",
 			"public/scripts/*.js",
 			"test/unit/*.js"
 		]
