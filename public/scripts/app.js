30ba3cd1edd3195679c8a447f8ab413b2582f842
Michał Dudak <michal.dudak@gmail.com>
2016-01-18 10:51:06 +0100

    State after day 1

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,7 @@
 (function(angular) {
 	"use strict";
 
-	var app = angular.module("app", ["ngRoute", "ngResource", "ngMessages", "gallery"]);
+	var app = angular.module("app", ["ngRoute"]);
 
 	app.config(function($routeProvider) {
 		$routeProvider
@@ -12,21 +12,6 @@
 			.when("/cart", {
 				templateUrl: "/views/shoppingCart.html",
 				controller: "shoppingCartController"
-			})
-			.when("/order", {
-				templateUrl: "/views/order.html",
-				controller: "orderController"
-			})
-			.when("/orderConfirmation/:id", {
-				templateUrl: "/views/orderConfirmation.html",
-				controller: "orderConfirmationController",
-				resolve: {
-					order: function(Order, $route) {
-						// Returning a promise ensures that Angular will wait for
-						//   the remote data before switching the view.
-						return Order.get({ id: $route.current.params.id }).$promise;
-					}
-				}
 			});
 	});
 
--- public/scripts/orderConfirmationController.js
+++
@@ -1,9 +0,0 @@
-(function(angular) {
-	"use strict";
-	
-	angular.module("app").controller("orderConfirmationController", function($scope, order) {
-		// The whole order-retreiving logic is placed in app.js (in the /orderConfirmation route definition).
-		$scope.order = order;
-	});
-	
-}(window.angular));
