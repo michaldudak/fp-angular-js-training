64d9f5df1fe5ffb3f0c36b12418da3dbff1f14b3
Michał Dudak <michal.dudak@gmail.com>
2016-04-18 15:34:54 +0200

    Removed cart persistence and events code

--- public/scripts/shoppingCart.js
+++ public/scripts/shoppingCart.js
@@ -2,34 +2,14 @@
 	"use strict";
 
 	angular.module("app").service("shoppingCart", function($rootScope) {
-		var cart = angular.fromJson(localStorage.getItem("cart") || "{}");
+		var cart = {};
 
 		this.addProduct = function(product) {
 			if (!cart[product.id]) {
 				cart[product.id] = { product: product, quantity: 1 };
 			} else {
 				cart[product.id].quantity += 1;
-			}
-
-			localStorage.setItem("cart", angular.toJson(cart));
-			$rootScope.$broadcast("cartContentsChanged");
-		};
-
-		this.removeAll = function() {
-			cart = {};
-			localStorage.setItem("cart", angular.toJson(cart));
-			$rootScope.$broadcast("cartContentsChanged");
-		};
-
-		this.getItemCount = function() {
-			var count = 0;
-			for (var id in cart) {
-				if (cart.hasOwnProperty(id)) {
-					count += cart[id].quantity;
-				}
-			}
-
-			return count;
+			}		
 		};
 
 		this.getProducts = function() {
@@ -38,21 +18,6 @@
 			for (var id in cart) {
 				if (cart.hasOwnProperty(id)) {
 					contents.push(cart[id]);
-				}
-			}
-
-			return contents;
-		};
-
-		this.getProductsForOrder = function() {
-			var contents = [];
-
-			for (var id in cart) {
-				if (cart.hasOwnProperty(id)) {
-					contents.push({
-						productId: id,
-						quantity: cart[id].quantity
-					});
 				}
 			}
 
